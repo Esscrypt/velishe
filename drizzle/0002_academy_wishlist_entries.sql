36d8fe56f63238a9aef3d6eb4e85b7677122ad8e
Mihail Kirov <me@mikirov.xyz>
2026-02-10 21:08:54 +0200

    wip

---
+++ drizzle/0002_academy_wishlist_entries.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "academy_wishlist_entries" (
+  "id" serial PRIMARY KEY NOT NULL,
+  "email" text NOT NULL,
+  "phone_number" text NOT NULL,
+  "created_at" timestamp DEFAULT now() NOT NULL
+);
+
